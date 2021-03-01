7ba4757a273530bfa0aa31f7c7889ee3dc485e21
ymtyroni <lukasz.calka@protonmail.com>
2021-03-01 16:55:52 +0100

    Basic gui implementation

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from gui_ask1 import Ui_Dialog
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    app.exec()
